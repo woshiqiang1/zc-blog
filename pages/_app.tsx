fe42f655c80a9105545da44800e9f0870fd1c9c1
MEGVII-INC\zoucheng02 <775106512@qq.com>
2023-08-06 13:35:06 +0800

    chore: remove .babelrc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ThemeProvider } from 'next-themes'
-import "core-js/stable";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
