20b579d39d85bec5d5601cc72141e0bdd4c2249d
MEGVII-INC\zoucheng02 <775106512@qq.com>
2023-08-06 10:30:18 +0800

    test: ie 11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ThemeProvider } from 'next-themes'
+import "core-js/stable";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
